f56e958db1fc5c6db1cfbe6405a37a0822b85b36
Sachinravi369 <s4970578@gmail.com>
2024-10-11 21:26:18 +0530

    Create Sample.cpp

---
+++ Sample.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class sample()
+{
+  int n;
+public:
+void sample();
+}
+void sample::sample()
+{
+  cout<<"enter a number";
+cin>>n;
+cout<<"number you entered "<<n;
+}
+void main()
+{
+  sample a;
+return;
+}
